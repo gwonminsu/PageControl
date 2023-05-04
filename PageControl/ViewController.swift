ca8ab430f82e0f9487af287d3e7fe13efb6d4198
권민수 <kd0615@naver.com>
2023-05-04 12:13:19 +0900

    페이지 컨트롤 연습

--- PageControl/ViewController.swift
+++ PageControl/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var images = [ "cat.jpeg", "cat2.png", "devilcat.jpeg", "fanxycat.jpeg", "hmmchitcat.jpeg", "ironcat.jpeg" ]
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        imgView.image = UIImage(named: images[0])
     }
 
-
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
 }
 
